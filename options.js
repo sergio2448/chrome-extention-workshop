bf3cc41d8c34bc01a34767f5ca6f944756e90d76
sergio2448 <84557725+sergio2448@users.noreply.github.com>
2024-07-27 11:45:44 -0500

    Add save_options function

---
+++ options.js
@@ -0,0 +1,6 @@
+document.getElementById('save').addEventListener('click', save_options);
+
+function save_options() {
+  const API_KEY = document.getElementById('key').value;
+  chrome.storage.sync.set({ API_KEY });
+}
